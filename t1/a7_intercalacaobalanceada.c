e5400aef8ff5ea56d6f60ef9223110bfdd45b742
Thalisson Forte <thalisson1forte@gmail.com>
2019-05-10 20:05:21 -0300

    Problema de EOF
    Não encontra final do arquivo no momento certo (demora mais uma execução), o que gera valores repetidos nas fitas.

--- t1/a7_intercalacaobalanceada.c
+++ t1/a7_intercalacaobalanceada.c
@@ -5,7 +5,7 @@
 #include "quicksort.h"
 
 #define RAM 5
-#define NUM_NUMS 16
+#define NUM_NUMS 22
 #define NUM_CAMINHOS 3
 #define VALOR_GRANDE 99999
 
@@ -69,62 +69,98 @@ int ler_menor_arquivos(int deslocamento_base, int arquivo_base, int *deslocament
   int conteudo_1, conteudo_2, conteudo_3, menor = VALOR_GRANDE;
   int *arq_menor;
 
-  // FAZER COM QUE OS ARQUIVOS LEIAM NAS POSIÇÕES CORRETAS
-  fseek(arqstemp[arquivo_base], *deslocamento_1 * sizeof(int), 0);
-  fseek(arqstemp[arquivo_base + 1], *deslocamento_2 * sizeof(int), 0);
-  fseek(arqstemp[arquivo_base + 2], *deslocamento_3 * sizeof(int), 0);
+  // FAZER COM QUE OS ARQUIVOS LEIAM NAS POSIÇÕES CORRETAS SE NÃO TIVEREM CHEGADO AO FINAL
+  if(!feof(arqstemp[arquivo_base])) fseek(arqstemp[arquivo_base], (deslocamento_base + *deslocamento_1) * sizeof(int), 0);
+  if(!feof(arqstemp[arquivo_base+1])) fseek(arqstemp[arquivo_base + 1], (deslocamento_base + *deslocamento_2) * sizeof(int), 0);
+  if(!feof(arqstemp[arquivo_base+2])) fseek(arqstemp[arquivo_base + 2], (deslocamento_base + *deslocamento_3) * sizeof(int), 0);
 
-  printf("\tDeslocamento 1:%i 2:%i 3:%i\n", *deslocamento_1, *deslocamento_2, *deslocamento_3);
+  
+
+  printf("Deslocamento %i %i %i\n", *deslocamento_1, *deslocamento_2, *deslocamento_3);
   // CONTEUDO PRIMEIRO ARQUIVO
-  if(*deslocamento_1 < deslocamento_base + 3){
+  if(*deslocamento_1 < 5 && !feof(arqstemp[arquivo_base])){
     fread(&conteudo_1, sizeof(int), 1, arqstemp[arquivo_base]);
-    printf("Cont 1: %i\n",conteudo_1);
+    printf("0: %i\n",conteudo_1);
     if(conteudo_1 < menor){
       menor = conteudo_1;
       arq_menor = deslocamento_1;
     }
+
   }
   // CONTEUDO SEGUNDO ARQUIVO
-  if(*deslocamento_2 < deslocamento_base + 3){
+  if(*deslocamento_2 < 5 && !feof(arqstemp[arquivo_base+1])){
     fread(&conteudo_2, sizeof(int), 1, arqstemp[arquivo_base + 1]);
-    printf("Cont 2: %i\n",conteudo_2);
+    printf("1: %i\n",conteudo_2);
     if(conteudo_2 < menor){
       menor = conteudo_2;
       arq_menor = deslocamento_2;
     }
+
   }
   // CONTEUDO TERCEIRO ARQUIVO
-  if(*deslocamento_3 < deslocamento_base + 3){
+  if(*deslocamento_3 < 5 && !feof(arqstemp[arquivo_base+2])){
     fread(&conteudo_3, sizeof(int), 1, arqstemp[arquivo_base + 2]);
-    printf("Cont 3: %i\n",conteudo_3);
+    printf("2: %i\n",conteudo_3);
     if(conteudo_3 < menor){
       menor = conteudo_3;
       arq_menor = deslocamento_3;
     }
   }
 
+
   // ERRO
   if (menor == VALOR_GRANDE) return -1;
-  printf("Menor: %i",menor);
+  printf("Menor: %i\n\n",menor);
   // ANDA O PONTEIRO DO ARQUIVO MENOR
   (*arq_menor)++;
+
+  
+
+  if(feof(arqstemp[arquivo_base])) printf("ARQUIVO 3 ACABOU!\n");
+  if(feof(arqstemp[arquivo_base+1])) printf("ARQUIVO 4 ACABOU!\n");
+  if(feof(arqstemp[arquivo_base+2])) printf("ARQUIVO 5 ACABOU!\n");
+
   // RETORNA MENOR VALOR
   return menor;
 }
 
 void intercalacao_balanceada(int num_caminhos, FILE **arqstemp, char *nome_arq){
 
-  int buffer[RAM], lidos;
-  int i_1 = 0, i_2 = 0, i_3 = 0;
+  int buffer[RAM], lidos, tamanho_memoria = 0, novos_arquivos, auxiliar = 0, indice_arquivo, idx_novos = 0;
+  int deslocamento_base = 0;
+  int i_1 = 0, i_2 = 0, i_3 = 0; // POSIÇÕES QUE ESTÃO SENDO LIDAS
 
   // ARQUIVOS JA EXISTENTES
   abre_arqs_temp(0,num_caminhos,nome_arq,arqstemp,"rb");
   // NOVOS ARQUIVOS (3 a 5)
   abre_arqs_temp(num_caminhos, num_caminhos*2, nome_arq, arqstemp, "wb");
+  auxiliar = 0;
+  novos_arquivos = 3;
 
-  printf("\t%i\n",ler_menor_arquivos(0, 0, &i_1, &i_2, &i_3, arqstemp, nome_arq));
-  printf("\t%i\n",ler_menor_arquivos(0, 0, &i_1, &i_2, &i_3, arqstemp, nome_arq));
-  printf("\t%i\n",ler_menor_arquivos(0, 0, &i_1, &i_2, &i_3, arqstemp, nome_arq));
+  while(idx_novos < 5){ // RODADAS DE ORDENAÇÃO TENHO QUE ARRUMAR (5 PQ TO USANDO 25 NUMEROS E 25/RAM = 5)
+    while(tamanho_memoria < RAM){
+      buffer[tamanho_memoria] = ler_menor_arquivos(deslocamento_base, 0, &i_1, &i_2, &i_3, arqstemp, nome_arq);
+      tamanho_memoria++;
+    } // BUFFER CHEIO (5 números)
+
+    indice_arquivo = num_caminhos + auxiliar;
+    fwrite(buffer, sizeof(int)*RAM, 1, arqstemp[indice_arquivo]);
+    // ATUALIZAR INFORMACOES PRA PROXIMO ARQUIVO
+    idx_novos++; // CONTAR MAIS UMA RODADA FEITA - TENHO QUE ARRUMAR ISSO PQ NAO PODE SER SEMPRE <5
+    auxiliar++; // PRÓXIMO ARQUIVO
+    auxiliar = auxiliar % num_caminhos; // LIMITAR ARQUIVOS ( 0 1 2 0 1 2 0 )
+    printf("Buffer cheio! Passamos para a proxima fita: %i\tIndice do arquivo: %i\n\n",auxiliar, auxiliar+num_caminhos);
+    if(auxiliar == 0){ // AUX == 0 SIGNIFICA QUE RETORNOU A FITA INICIAL
+      deslocamento_base += 5;
+      i_1 = 0;
+      i_2 = 0;
+      i_3 = 0;
+    }
+    tamanho_memoria = 0; // ZERA MEMORIA (buffer)
+  }
+
+
+
 
   fecha_arqs(num_caminhos*2,arqstemp);
 }
